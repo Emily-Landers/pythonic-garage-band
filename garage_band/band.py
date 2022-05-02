8aca25fb5628c80adbe23350140396411c055ad9
Emily Landers <emily.j.landers@gmail.com>
2022-05-01 21:10:06 -0400

    complete

--- garage_band/band.py
+++ garage_band/band.py
@@ -1,63 +1,53 @@
+
 class Band():
-    
-    band_list = [];
-    
-    def __init__(self, name, members=None):
+
+    instances = []
+    def __init__(self , name, members):
         self.name = name
         self.members = members
-        Band.band_list.append(self.name)
-        
-    def __str__(self):
-        return f"The band {self.name}"
-        
-    def __repr__(self):
-        return f"Band instance. name={self.name}, members={self.members}"
-    
-    @classmethod
-    
-    def to_list(cls):
-        return cls.band_list
-    
+        Band.instances.append(self)
+
     def play_solos(self):
-        solos = []
+        play_solos_list = []
         for member in self.members:
-            solos.append(member.play_solo())
-            return solos
-    
-    
-class Musician(Band):
-    
-    def __init__(self, name, instrument):
+            play_solos_list.append(member.play_solo())
+        return play_solos_list
+
+    @classmethod
+    def to_list(cls):
+        return cls.instances
+
+class Musician():
+    instrument = ""
+    title = ""
+    solo = ""
+
+    def __init__(self, name):
         self.name = name
-        self.instrument = instrument
-        
-    def __repr__(self):
-        return f"{self.__class__.__name__} instance. Name = {self.name}" 
-    
+
     def __str__(self):
         return f"My name is {self.name} and I play {self.instrument}"
-    
-class Guitarist(Musician):
-    def __init__(self, name):
-        super().__init__(name, "guitar")
+
+    def __repr__(self):
+        return f"{self.title} instance. Name = {self.name}"
 
     def get_instrument(self):
-        return self.instrument
+        return f"{self.instrument}"
 
     def play_solo(self):
-        return "this doesnt translate well over text"
+        return f"{self.solo}"
 
-#class Bassist(Musician):
-    #pass
+class Guitarist(Musician):
+    instrument = "guitar"
+    title = "Guitarist"
+    solo = "face melting guitar solo"
 
-#class Drummer(Musician):
-    #pass
+class Bassist(Musician):
+    instrument = "bass"
+    title = "Bassist"
+    solo = "bom bom buh bom"
 
-
-
-#class Boxer: 
-#def __init__(self):
-#self.name = "unknown"
-
-#ef greet(self):
-#return f"The names {self.name}."
+class Drummer(Musician):
+    instrument = "drums"
+    title = "Drummer"
+    solo = "rattle boom crash"
